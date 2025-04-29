919e9f82b6e727f0826dc4d5c63d1983ffbaac42
danishanr <danishaanr@gmail.com>
2025-04-29 18:32:06 +0700

    Membuat Procedure

--- oop.cpp
+++ oop.cpp
@@ -3,10 +3,16 @@ using namespace std;
 
 class Mahasiswa{
     public :
-    
+
     int nim;
     string nama;
     float nilai;
+
+    void printData(){
+        cout << "NIM: " << nim << endl;
+        cout << "Nama: " << nama << endl;
+        cout << "Nilai: " << nilai << endl;
+    }
 
     
 
